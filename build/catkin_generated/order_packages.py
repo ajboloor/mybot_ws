ba36998fe7f4bf951667913283f294af729a882c
ajboloor <adith94@gmail.com>
2018-03-15 12:31:11 -0500

    testing git

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/adith-jb/projects/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/adith-jb/projects/test_ws/devel;/home/adith-jb/projects/mybot_ws/devel;/home/adith-jb/projects/picar_ws/devel;/opt/ros/kinetic".split(';') if "/home/adith-jb/projects/test_ws/devel;/home/adith-jb/projects/mybot_ws/devel;/home/adith-jb/projects/picar_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/adith-jb/projects/test_ws/devel/lib:/home/adith-jb/projects/picar_ws/devel/lib:/opt/ros/kinetic/lib"
+export PKG_CONFIG_PATH="/home/adith-jb/projects/test_ws/devel/lib/pkgconfig:/home/adith-jb/projects/picar_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig"
+export PWD="/home/adith-jb/projects/mybot_ws/build"
